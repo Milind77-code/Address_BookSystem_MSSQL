c8757f3d4927f064fc945fd45cd018b715350129
Milind_77 <milinddhote77@gmail.com>
2021-08-15 14:46:33 +0530

    [Milind] Add:Edit Existing Contact Using First_Name.

---
+++ EditUsingFirstName.sql
@@ -0,0 +1,8 @@
+update [dbo].[address_book] set city = 'Beed' 
+where first_name = 'Milind';
+
+update [dbo].[address_book] set address = 'nagadara' 
+where first_name = 'Dipak';
+
+update [dbo].[address_book] set city = 'Californiya' 
+where first_name = 'Sravesh';
