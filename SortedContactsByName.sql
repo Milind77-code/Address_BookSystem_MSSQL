fa34bda19a28c60213f9fac5c737c9926225899d
Milind_77 <milinddhote77@gmail.com>
2021-08-15 15:08:29 +0530

    [Milind] Add:Sort Contacts by First Name Using City.

---
+++ SortedContactsByName.sql
@@ -0,0 +1,8 @@
+select * from [dbo].[address_book]
+where city = 'Nagpur' order by first_name asc;
+
+select first_name, last_name from [dbo].[address_book]
+where city = 'Pune' order by first_name asc;
+
+select * from [dbo].[address_book]
+where city = 'Mumbai' order by first_name asc;
