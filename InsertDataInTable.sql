3c3a66aad429a81de6a4256c26b7ba731e3ba235
Milind_77 <milinddhote77@gmail.com>
2021-08-15 14:39:49 +0530

    [Milind] Add:Insert new Records in addressBook.

---
+++ InsertDataInTable.sql
@@ -0,0 +1,5 @@
+insert into dbo.address_book values
+('Milind', 'Dhote', 'Shivaji_Choak', 'Nagpur', 'Maharashtra', 431517, '9130918545', 'milind77@gmail.com'),
+('Sarvesh', 'Motlag', 'Dighi', 'Pune', 'Maharashtra', 411014, '9245632154', 'sh98@gmail.com'),
+('Dipak', 'Nagargoje', 'nagar', 'pune', 'Maharashtra', 431347, '9112365478', 'dpk@gmail.com'),
+('Mahesh', 'Kande', 'parli', 'Mumbai', 'Maharashtra', 431434, '9987456120', 'mkas@gmail.com')
